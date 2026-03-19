88ec3d8f03b0b1d2c4b9ba5deff324c473eef74c
nicolasgarcia06 <805@alumnos.fpsumma.es>
2026-03-19 10:36:54 +0100

    hola

---
+++ src/main/java/lumarasports/LumarasportsApplication.java
@@ -0,0 +1,13 @@
+package lumarasports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumarasportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumarasportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lumarasports/LumarasportsApplicationTests.java
@@ -0,0 +1,13 @@
+package lumarasports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumarasportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
